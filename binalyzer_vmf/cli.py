06426681d67367015abc9118179cca7e64c09461
Denis Vasilik <contact@denisvasilik.com>
2021-03-04 19:26:13 +0100

    Configure option settings

--- binalyzer_vmf/cli.py
+++ binalyzer_vmf/cli.py
@@ -15,14 +15,13 @@ from binalyzer import (
 
 
 @click.command()
-@click.option('--input-file', '-i', type=ExpandedFile("rb"), help='The binary file to convert.')
-@click.option('--output-file', '-o', type=ExpandedFile("wb"), help='The VMF file to create.')
-@click.option('--start-offset', '-s', type=BasedIntParamType(), help='The VMF file to create.')
+@click.option('--input-file', '-i', required=True, type=ExpandedFile("rb"), help='The binary file to convert.')
+@click.option('--output-file', '-o', required=True, type=ExpandedFile("w"), help='The VMF file to create.')
+@click.option('--start-offset', '-s', type=BasedIntParamType(), default="0x00", show_default=True, help='The VMF file to create.')
 def vmf(input_file, output_file, start_offset):
     """Convert a binary file to a VMF file.
     """
     binalyzer = Binalyzer(data=input_file)
-
-    print(f"@{start_offset:06X}")
+    output_file.write(f"@{start_offset:06X}\n")
     for data_byte in binalyzer.template.value:
-        print(f"{data_byte:02X}")
+        output_file.write(f"{data_byte:02X}\n")
