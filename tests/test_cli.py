1d0b4881c2c27867aa154e8f84c7951528855290
Denis Vasilik <contact@denisvasilik.com>
2021-03-06 23:14:08 +0100

    Add unit tests

---
+++ tests/test_cli.py
@@ -0,0 +1,43 @@
+import pytest
+import os
+
+from click.testing import CliRunner
+
+from binalyzer import Template, TemplateAutoCompletion, cli
+
+TESTS_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_vmf_without_arguments():
+    result = CliRunner().invoke(cli, ["vmf"])
+
+    assert (
+        result.output
+        == 'Usage: binalyzer vmf [OPTIONS]\nTry "binalyzer vmf --help" for help.\n\n'
+           'Error: Missing option "--input-file" / "-i".\n'
+    )
+    assert result.exit_code == 2
+
+
+def test_vmf_help():
+    result = CliRunner().invoke(cli, ["vmf", "--help"])
+    assert (
+        result.output
+        == 'Usage: binalyzer vmf [OPTIONS]\n\n  Convert a binary file to a VMF '
+           'file.\n\nOptions:\n  -i, --input-file FILENAME   The binary file to '
+           'convert.  [required]\n  -o, --output-file FILENAME  The VMF file to '
+           'create.  [required]\n  -s, --start-offset INTEGER  Specifes the start '
+           'offset  [default: 0x00]\n  --help                      Show this '
+           'message and exit.\n'
+    )
+    assert result.exit_code == 0
+
+
+def test_vmf_missing_input_file():
+    result = CliRunner().invoke(cli, ["vmf", "--input-file", "resources/WasmFpgaModule.wasm"])
+    assert(
+        result.output ==
+        'Usage: binalyzer vmf [OPTIONS]\nTry "binalyzer vmf --help" for help.\n\n'
+        'Error: Missing option "--output-file" / "-o".\n'
+    )
+    assert result.exit_code == 2
