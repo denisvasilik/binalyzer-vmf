97ce7dd1bd28b4cf4a28f9ca95305dc4edfb8b20
Denis Vasilik <contact@denisvasilik.com>
2021-03-06 23:14:35 +0100

    Fix start offset help

--- binalyzer_vmf/cli.py
+++ binalyzer_vmf/cli.py
@@ -17,7 +17,7 @@ from binalyzer import (
 @click.command()
 @click.option('--input-file', '-i', required=True, type=ExpandedFile("rb"), help='The binary file to convert.')
 @click.option('--output-file', '-o', required=True, type=ExpandedFile("w"), help='The VMF file to create.')
-@click.option('--start-offset', '-s', type=BasedIntParamType(), default="0x00", show_default=True, help='The VMF file to create.')
+@click.option('--start-offset', '-s', type=BasedIntParamType(), default="0x00", show_default=True, help='Specifes the start offset')
 def vmf(input_file, output_file, start_offset):
     """Convert a binary file to a VMF file.
     """
