2cfd8a4e67da01b52a0b3a7f62bc30a2070cc3c0
Denis Vasilik <contact@denisvasilik.com>
2021-03-07 09:08:01 +0100

    Add tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -34,10 +34,51 @@ def test_vmf_help():
 
 
 def test_vmf_missing_input_file():
-    result = CliRunner().invoke(cli, ["vmf", "--input-file", "resources/WasmFpgaModule.wasm"])
+    result = CliRunner().invoke(
+        cli, ["vmf", "--input-file", "resources/WasmFpgaModule.wasm"])
     assert(
         result.output ==
         'Usage: binalyzer vmf [OPTIONS]\nTry "binalyzer vmf --help" for help.\n\n'
         'Error: Missing option "--output-file" / "-o".\n'
     )
     assert result.exit_code == 2
+
+
+def test_vmf_default_offset():
+    result = CliRunner().invoke(cli, [
+        "vmf",
+        "--input-file",
+        "resources/WasmFpgaModule.wasm",
+        "--output-file",
+        "/tmp/WasmFpgaModule.vmf",
+    ])
+    assert result.output == ""
+    assert result.exit_code == 0
+
+
+def test_vmf_offset_hex():
+    result = CliRunner().invoke(cli, [
+        "vmf",
+        "--input-file",
+        "resources/WasmFpgaModule.wasm",
+        "--output-file",
+        "/tmp/WasmFpgaModule.vmf",
+        "--start-offset",
+        "0x300000",
+    ])
+    assert result.output == ""
+    assert result.exit_code == 0
+
+
+def test_vmf_offset_decimal():
+    result = CliRunner().invoke(cli, [
+        "vmf",
+        "--input-file",
+        "resources/WasmFpgaModule.wasm",
+        "--output-file",
+        "/tmp/WasmFpgaModule.vmf",
+        "--start-offset",
+        "256",
+    ])
+    assert result.output == ""
+    assert result.exit_code == 0
